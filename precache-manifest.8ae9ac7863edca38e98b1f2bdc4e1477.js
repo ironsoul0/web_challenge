13840622038c2f05093f5c75405ffb452c3bc963
Temirzhan Yussupov <ironsoul@Temirzhans-MBP.mshome.net>
2019-01-14 00:10:55 +0600

    Updates

---
+++ precache-manifest.8ae9ac7863edca38e98b1f2bdc4e1477.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "28b0638b1713faa57edd",
+    "url": "/web_challenge/static/css/main.e20ade24.chunk.css"
+  },
+  {
+    "revision": "28b0638b1713faa57edd",
+    "url": "/web_challenge/static/js/main.28b0638b.chunk.js"
+  },
+  {
+    "revision": "6fd58a21866498878629",
+    "url": "/web_challenge/static/js/1.6fd58a21.chunk.js"
+  },
+  {
+    "revision": "a89953a9286b0f7e4330",
+    "url": "/web_challenge/static/js/runtime~main.a89953a9.js"
+  },
+  {
+    "revision": "89aed482e2142233c5c3aa0cd274852c",
+    "url": "/web_challenge/index.html"
+  }
+];
